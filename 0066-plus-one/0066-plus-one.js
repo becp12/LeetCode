269ad30105b6761b8eb9d39bbd635100f9d1e22b
Rebecca Preece <97765304+becp12@users.noreply.github.com>
2022-10-26 10:26:47 -0700

    Time: 103 ms (33.59%), Space: 42.4 MB (15.37%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -2,27 +2,6 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-// var plusOne = function(digits) {
-//     let arr = [...digits];
-//     arr = parseInt(arr.join(""));
-//     arr += 1;
-//     newArr = Array.from(arr.toString()).map(Number);
-//     console.log(newArr);
-// };
-
-// arr.split('');
-    // console.log(arr);
-    // 
-    // newArr.pop();
-    // newArr.push(newNum);
-    // console.log(newArr);
-    // const newNum = arr[arr.length - 1] + 1;
-    // const newArr = arr.splice(arr.length-1, 1, newNum)
-    // console.log(newArr);
-// Array.from(n.toString()).map(Number);
-// const n = 123456;
-// 
-// // [1, 2, 3, 4, 5, 6]
 
 var plusOne = function(digits) {
   const str = digits.join("")

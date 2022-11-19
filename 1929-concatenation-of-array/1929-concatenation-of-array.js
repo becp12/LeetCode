d5c97c07378e28dc03663e6159f80245dbb2d952
Rebecca Preece <97765304+becp12@users.noreply.github.com>
2022-11-19 11:47:17 +1100

    Time: 92 ms (83.89%), Space: 46.1 MB (27.73%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -3,17 +3,18 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    let ans = [];
-    let n = nums.length;
-    while (ans.length != n) {
-        for (let i = 0; i < n; i++) {
-            ans.push(nums[i]);
-        }
-    }
-    while (ans.length != 2*n) {
-        for (let i = 0; i < n; i++) {
-            ans.push(nums[i]);
-        }
-    }
-    return ans;
+    // let ans = [];
+    // let n = nums.length;
+    // while (ans.length != n) {
+    //     for (let i = 0; i < n; i++) {
+    //         ans.push(nums[i]);
+    //     }
+    // }
+    // while (ans.length != 2*n) {
+    //     for (let i = 0; i < n; i++) {
+    //         ans.push(nums[i]);
+    //     }
+    // }
+    // return ans;
+    return [...nums, ...nums]
 };

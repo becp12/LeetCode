8e4f640c6179cfc400d3a55e380205689f28baf7
Rebecca Preece <97765304+becp12@users.noreply.github.com>
2022-12-02 08:59:51 +1100

    Time: 104 ms (44.42%), Space: 42.4 MB (49.84%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    let result = [];
+
+    let maxCandies = 0;
+	for (let i = 0; i < candies.length; i++) {
+		if (candies[i] > maxCandies) {
+			maxCandies = candies[i];
+		}
+	}
+    
+    for (let i = 0; i < candies.length; i++) {
+        let currentCandies = candies[i] + extraCandies;
+        // if (i === 0) {
+        //     result.push(true);
+        // } else {
+            if (currentCandies >= maxCandies) {
+                result.push(true);
+            } else {
+                result.push(false);
+            }
+        // }
+    }
+    return result;
+};
